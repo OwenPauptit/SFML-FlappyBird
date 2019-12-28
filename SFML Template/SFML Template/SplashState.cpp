8ee72274869f592a10c2e814f141938076d26ac9
OwenPauptit <55768469+OwenPauptit@users.noreply.github.com>
2019-12-28 15:46:24 +0000

    Added comments
    Commented in SplashState.cpp

--- SFML Template/SFML Template/SplashState.cpp
+++ SFML Template/SFML Template/SplashState.cpp
@@ -12,6 +12,7 @@ namespace Aesel {
 	}
 
 	void SplashState::Init() {
+		// Load Splash state texture and set it to the sprite _background
 		this->_data->assets.LoadTexture("Splash State Background", SPLASH_SCREEN_BACKGROUND_FILEPATH);
 
 		_background.setTexture(this->_data->assets.GetTexture("Splash State Background"));
@@ -19,6 +20,7 @@ namespace Aesel {
 
 	}
 
+	// Check if close button on window has been pressed
 	void SplashState::HandleInput() {
 		sf::Event event;
 		while (this->_data->window.pollEvent(event)) {
@@ -28,6 +30,7 @@ namespace Aesel {
 		}
 	}
 
+	// If more time has passed than the time allocated in DEFINITIONS.hpp, then the main menu will be loaded
 	void SplashState::Update(float dt) {
 		if (_clock.getElapsedTime().asSeconds() > SPLASH_STATE_SHOW_TIME) {
 			std::cout << "Go to main menu" << std::endl;
@@ -37,6 +40,8 @@ namespace Aesel {
 		}
 	}
 
+
+	//Drawing the background to the screen
 	void SplashState::Draw(float dt) {
 		this->_data->window.clear(sf::Color::Red);
 
