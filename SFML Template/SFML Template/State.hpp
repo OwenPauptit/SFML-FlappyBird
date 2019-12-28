b3ab6513535f568e67ecb2e56589d73fcb0c46e5
OwenPauptit <55768469+OwenPauptit@users.noreply.github.com>
2019-12-28 13:42:36 +0000

    Added Game loop - finished game engine
    Created Game.cpp, Game.hpp
    Some minor modifications elsewhere

--- SFML Template/SFML Template/State.hpp
+++ SFML Template/SFML Template/State.hpp
@@ -8,7 +8,7 @@ namespace Aesel {
 		virtual void Init() = 0;
 
 		virtual void HandleInput() = 0;
-		virtual void Update() = 0;
+		virtual void Update(float dt) = 0;
 		virtual void draw(float dt) = 0; //dt is delta time, the time between frames, so whether animations need to be drawn or not can be calculated
 
 		virtual void Pause( ) { }
